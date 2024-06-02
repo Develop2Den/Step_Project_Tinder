c408a43013a011e83a69a4c09c67ff57d521aea3
AlexandrIsai <artat@ukr.net>
2024-06-02 20:27:00 +0200

    jar

--- src/main/java/utils/ResourcesOps.java
+++ src/main/java/utils/ResourcesOps.java
@@ -1,18 +1,38 @@
 package utils;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ResourcesOps {
-    public static String dirUnsafe(String prefix) {
+    public static List<String> dirUnsafe(String prefix) {
         try {
             URL resourceUrl = ResourcesOps.class.getClassLoader().getResource(prefix);
             if (resourceUrl == null) {
                 throw new IllegalArgumentException("Resource not found: " + prefix);
             }
-            Path resourcePath = Paths.get(resourceUrl.toURI());
-            return resourcePath.toString();
+            URI resourceUri = resourceUrl.toURI();
+
+            Path resourcePath;
+            if ("jar".equals(resourceUri.getScheme())) {
+                try (FileSystem fs = FileSystems.newFileSystem(resourceUri, Collections.emptyMap())) {
+                    resourcePath = fs.getPath(prefix);
+                }
+            } else {
+                resourcePath = Paths.get(resourceUri);
+            }
+
+            try (Stream<Path> paths = Files.walk(resourcePath, 1)) {
+                return paths
+                        .filter(Files::isRegularFile)
+                        .map(Path::toString)
+                        .collect(Collectors.toList());
+            }
         } catch (Exception ex) {
             throw new RuntimeException("Error while getting resource directory: " + prefix, ex);
         }
